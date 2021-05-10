d8885ed528dda7d3dd7c3b7a3a2fe7a50d88d403
krifty <kaitlynmariejones@gmail.com>
2021-05-10 12:23:51 -0700

    OperatorBttn Workgit add -A operator button work and display correctly

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let originalNum = 0
 let displayNum = ""
 let operator = ""
-let resultNum = 0
 
 display(0);
 
@@ -18,48 +17,51 @@ function clearBttn(){
     displayNum = ""
     operator = ""
     originalNum = 0
-    // console.log(displayNum);
     display(displayNum);
 }
 
 function operatorBttn(operatorBttn){
-    switch(operatorBttn){
-        case "+" : console.log("plus"); break;
-        case "-" : console.log("minus"); break;
-        case "/" : console.log("divid"); break;
-        case "*" : console.log("miltiply"); break;
-        case "=" : console.log("="); break;
-        default : console.log('fail');
-
-
+   
+    if (operator == "") {
+        originalNum = Number(displayNum);
+        displayNum = ""
+        operator = operatorBttn;
+    } else{
+        switch(operator){
+            case "+" : add(); break;
+            case "-" : subtract(); break;
+            case "/" : divide(); break;
+            case "*" : multiply(); break;
+            case "=" : equal(); break;
+            default : console.log('fail');
+        }
+        operator = operatorBttn
     }
-    
-   
-   
-   
-   
-   
-   
-    // if (!operator == "") {
-    //     console.log("error")
-  
-    operator = operatorBttn
-    add();
-    console.log(operator);
-   
-        
-    
 }
 
 function display(input){
     let solutionDisplay = document.querySelector("#solution-container");
     solutionDisplay.textContent = input;
 }
-
-function add(){
-    let numHolder = Number(displayNum);
-    let result = numHolder + originalNum;
+function calcDisplay(result){
     originalNum = result
     display(result);
     displayNum = ""
+}
+
+function add(){
+    calcDisplay(Number(displayNum) + originalNum);
+}
+
+function subtract(){
+    calcDisplay(originalNum - displayNum);
+}
+function divide(){
+    calcDisplay(originalNum / displayNum);
+}
+function multiply(){
+    calcDisplay(displayNum * originalNum);
+}
+function equal(){
+    calcDisplay(originalNum);
 }
