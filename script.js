f1a43870d37485083b25b86ff671795c6064e852
krifty <kaitlynmariejones@gmail.com>
2021-05-10 11:02:55 -0700

    display() created clicking num and clear bttn now display

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 let diplayNum = ""
 let operator = ""
 
+display(0);
 
 function numBttn(num){
     diplayNum += num
+    if (diplayNum.charAt(0) == 0 && diplayNum.length > 1) diplayNum = diplayNum.substr(1,1);
     console.log(num);
     console.log(diplayNum);
+    display(diplayNum);
 }
 
 function clearBttn(){
     diplayNum = ""
     operator = ""
     console.log(diplayNum);
+    display(diplayNum);
 }
 
 function operatorBttn(operatorBttn){
     if (!operator == "") console.log("error");
     operator = operatorBttn
     console.log(operator);
+}
+
+function display(input){
+    let solutionDisplay = document.querySelector("#solution-container");
+    solutionDisplay.textContent = input;
 }
