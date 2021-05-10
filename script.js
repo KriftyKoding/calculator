22957a77e6ce6e1c1bcc1d0cea30ab6b351e12b1
krifty <kaitlynmariejones@gmail.com>
2021-05-10 11:44:52 -0700

    OperatorBttn Update operatorBttn() now recognize which bttn you select it also correctly adds 2 numbers together

--- script.js
+++ script.js
@@ -1,30 +1,65 @@
-let diplayNum = ""
+let originalNum = 0
+let displayNum = ""
 let operator = ""
+let resultNum = 0
 
 display(0);
 
 function numBttn(num){
-    diplayNum += num
-    if (diplayNum.charAt(0) == 0 && diplayNum.length > 1) diplayNum = diplayNum.substr(1,1);
-    console.log(num);
-    console.log(diplayNum);
-    display(diplayNum);
+    displayNum += num
+    // if (displayNum.charAt(0) == 0 && displayNum.length > 1) displayNum = displayNum.substr(1,1);
+    // console.log(num);
+    // console.log(displayNum);
+    // console.log(Number(5.5));
+    display(displayNum);
 }
 
 function clearBttn(){
-    diplayNum = ""
+    displayNum = ""
     operator = ""
-    console.log(diplayNum);
-    display(diplayNum);
+    originalNum = 0
+    // console.log(displayNum);
+    display(displayNum);
 }
 
 function operatorBttn(operatorBttn){
-    if (!operator == "") console.log("error");
+    switch(operatorBttn){
+        case "+" : console.log("plus"); break;
+        case "-" : console.log("minus"); break;
+        case "/" : console.log("divid"); break;
+        case "*" : console.log("miltiply"); break;
+        case "=" : console.log("="); break;
+        default : console.log('fail');
+
+
+    }
+    
+   
+   
+   
+   
+   
+   
+    // if (!operator == "") {
+    //     console.log("error")
+  
     operator = operatorBttn
+    add();
     console.log(operator);
+   
+        
+    
 }
 
 function display(input){
     let solutionDisplay = document.querySelector("#solution-container");
     solutionDisplay.textContent = input;
+}
+
+function add(){
+    let numHolder = Number(displayNum);
+    let result = numHolder + originalNum;
+    originalNum = result
+    display(result);
+    displayNum = ""
 }
